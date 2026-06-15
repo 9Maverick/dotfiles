8d91eb967f4bc4296a4c4154062617fa851b217e
9Maverick <46315519+9Maverick@users.noreply.github.com>
2026-06-15 15:08:34 +0300

    add kns install script

---
+++ .chezmoiscripts/run_after_install_kde_knewstuff.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+knsregistry_location="$HOME/.local/share/knewstuff3/"
+
+for knsregistry_file in $knsregistry_location*; do
+  knsregistry=$(basename ${knsregistry_file%.*})
+
+  providers=($(grep -oP "<providerid>\K\S+(?=<)" $knsregistry_file))
+  items_ids=($(grep -oP "<id>\K\S+(?=<)" $knsregistry_file))
+  if [[ ${#providers[@]} != ${#items_ids[@]} ]]; then
+    echo ">>> ERROR: mismatch of providers and ids number:"
+    echo ">>> providers count: ${#providers[@]}"
+    echo ">>> providers items:" "${providers[@]}"
+    echo ">>> ids count: ${#items_ids[@]}"
+    echo ">>> ids items:" "${items_ids[@]}"
+    echo ">>> filename: $knsregistry_file"
+  fi
+  count=${#items_ids[@]}
+
+  for ((i = 0; i < $count; i++)); do
+    item_provider=${providers[i]}
+    item_id=${items_ids[i]}
+
+    url="kns://$knsregistry.knsrc/$item_provider/$item_id"
+    knshandler $url
+  done
+
+done
